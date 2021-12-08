2a9040c2b3106292f42caf224863d38692ffc333
이영림 <rm1113@naver.com>
2021-12-08 18:13:16 +0900

    ex02

--- src/com/javaex/ex02/Friend.java
+++ src/com/javaex/ex02/Friend.java
@@ -5,12 +5,51 @@ public class Friend {
     private String name;
     private String hp;
     private String school;
+    
+    public Friend() {}
 	public Friend(String name, String hp, String school) {
 		super();
 		this.name = name;
 		this.hp = hp;
 		this.school = school;
 	}
+	
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getHp() {
+		return hp;
+	}
+	public void setHp(String hp) {
+		this.hp = hp;
+	}
+	public String getSchool() {
+		return school;
+	}
+	public void setSchool(String school) {
+		this.school = school;
+	}
+
+	
+	
+	@Override
+	public String toString() {
+		return "Friend [name=" + name + ", hp=" + hp + ", school=" + school + "]";
+	}
+	
+	public void showinfo() {
+		System.out.println("이름 : "+name+"\t"
+				+"핸드폰 : "+hp+"\t"
+				+"학교 : "+school);
+	}
+
+	
+	
+	
     
     
 
