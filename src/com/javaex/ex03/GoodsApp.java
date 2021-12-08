401674400160f20175a259e92e8313ef2c0f70de
이영림 <rm1113@naver.com>
2021-12-08 18:49:39 +0900

    ㅎㅎ 모르겟당!

--- src/com/javaex/ex03/GoodsApp.java
+++ src/com/javaex/ex03/GoodsApp.java
@@ -1,10 +1,53 @@
 package com.javaex.ex03;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class GoodsApp {
 
     public static void main(String[] args) {
-
+    	
+    	Scanner sc = new Scanner(System.in);
+    		
+    		List<Goods> gList = new ArrayList<Goods>();
+    		
+    		int count = 0;
+    		int sum = 0;
+    		
+    		System.out.println("상품을 입력해주세요(종료 q)");
+    		while(true) {
+    			
+        		String content = sc.nextLine();
+        		
+        		//입력받는경우
+        		String[] pinfo = content.split(",");
+        		
+        		//입력받은거 쪼개기
+        		String name = pinfo[0];
+        		int price = Integer.parseInt(pinfo[1]);
+        		count = Integer.parseInt(pinfo[2]);
+        		sum = sum + count;
+        		
+        		Goods pro = new Goods(name,price,count);
+        		
+        		gList.add(pro);
+        		
+        		if(content.equals("q")) {
+        			System.out.println("[입력완료]");
+        			System.out.println("==================");
+ //       			for( Goods g : gList ) {
+ //       				g.showinfo();
+ //       			}
+ //       			System.out.println("모든 상품의 갯수는 "+count+"개입니다.");
+        			break;
+        		}
+    		}
+    		
+    		
+       sc.close();
        
+    	
     }
 
 }
