27da4c4ce87b88710ed17f551dcdda4d15c0d2a4
이영림 <rm1113@naver.com>
2021-12-09 12:01:13 +0900

    ex02수정

--- src/com/javaex/ex02/FriendApp.java
+++ src/com/javaex/ex02/FriendApp.java
@@ -16,24 +16,14 @@ public class FriendApp {
     		
     		//문자열받고 공백으로 나누고 값을 넣어주기
     		
-    		//1
-    		String content = sc.nextLine(); //받고
-    		String[] finfo = content.split(" "); //나누고
-    		Friend f01 = new Friend(finfo[0],finfo[1], finfo[2]); //값
-    		fList.add(f01);
+    		for(int i=0; i<3; i++) {		
+    			String finfo = sc.nextLine();
+    			String[] fcon = finfo.split(" ");
+    			Friend f = new Friend(fcon[0],fcon[1],fcon[2]);
+    			fList.add(f);
+    		}
     		
-    		//2
-    		String content2 = sc.nextLine();
-    		String[] finfo2 = content2.split(" ");
-    		Friend f02 = new Friend(finfo2[0],finfo2[1], finfo2[2]); 
-    		fList.add(f02);
-    		
-    		//3
-    		String content3 = sc.nextLine();
-    		String[] finfo3 = content3.split(" ");
-    		Friend f03 = new Friend(finfo3[0],finfo3[1], finfo3[2]); 
-    		fList.add(f03);
-    		
+
     		for( Friend f : fList ) {
     			f.showinfo();
     		}
